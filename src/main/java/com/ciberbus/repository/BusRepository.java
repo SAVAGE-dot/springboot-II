72a18e403c035dd8c49bfdf352a57a001973d79a
Antonio Hans Ospina Tafur <tonymagia007@gmail.com>
2026-09-20 20:01:09 -0500

    P3: repositorios de maestros con consultas operativas (ciudad, turno, bus, conductor, ruta)

--- src/main/java/com/ciberbus/repository/BusRepository.java
+++ src/main/java/com/ciberbus/repository/BusRepository.java
@@ -1,8 +1,14 @@
 package com.ciberbus.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ciberbus.entity.Bus;
 
 public interface BusRepository extends JpaRepository<Bus, Integer> {
+
+    List<Bus> findByEstado(Integer estado);
+
+    boolean existsByNroPlaca(String nroPlaca);
 }
--- src/main/java/com/ciberbus/repository/ConductorRepository.java
+++ src/main/java/com/ciberbus/repository/ConductorRepository.java
@@ -1,8 +1,19 @@
 package com.ciberbus.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ciberbus.entity.Conductor;
 
 public interface ConductorRepository extends JpaRepository<Conductor, Integer> {
+
+    List<Conductor> findByEstado(Integer estado);
+
+    Optional<Conductor> findByNroDocumento(String nroDocumento);
+
+    boolean existsByNroDocumento(String nroDocumento);
+
+    boolean existsByCorreo(String correo);
 }
